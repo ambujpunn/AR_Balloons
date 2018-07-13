48194100178b1db8a427bf6808ee84b77aa124e8
Ambuj Punn <ambujpunn@gmail.com>
2018-07-12 18:54:35 -0700

    balloons being added

--- NextReality_Tutorial6/ViewController.swift
+++ NextReality_Tutorial6/ViewController.swift
@@ -22,12 +22,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        // 4.2
+        sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // 4.1
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        // 4.4
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        sceneView.addGestureRecognizer(gestureRecognizer)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -76,5 +83,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    // 4.5
+    @objc func tapped(gesture: UITapGestureRecognizer) {
+        let touchPosition = gesture.location(in: sceneView)
+        
+        // Translate that 2D point to a 3D point using hitTest (featurePoint), wherever there is a featurePoint, add a balloon there
+        let hitTestResults = sceneView.hitTest(touchPosition, types: .featurePoint)
+        
+        guard let hitTest = hitTestResults.first else {
+            return
+        }
+       
+        addBalloon(hitTest: hitTest)
+    }
+    
+    // 4.6
+    func addBalloon(hitTest: ARHitTestResult) {
+        let scene = SCNScene(named: "art.scnassets/red_balloon.dae")
+        let balloonNode = Balloon(scene: scene!)
+        balloonNode.position = SCNVector3(hitTest.worldTransform.columns.3.x, hitTest.worldTransform.columns.3.y, hitTest.worldTransform.columns.3.z)
+        
+        sceneView.scene.rootNode.addChildNode(balloonNode)
     }
 }
